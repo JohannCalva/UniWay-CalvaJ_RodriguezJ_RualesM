ce6a9393b67e73177a366040e1a27eb604ae84dc
Johann Calva <johanncalvacadme@gmail.com>
2025-04-26 15:45:18 -0500

    Add project files.

---
+++ UniWay-WebAppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UniWay_WebAppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
