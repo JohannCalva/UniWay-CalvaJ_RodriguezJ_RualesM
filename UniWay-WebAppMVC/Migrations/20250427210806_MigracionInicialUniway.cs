abe0cf64ccdce694ab0e41b401f0ef765cb20a1e
Johann Calva <johanncalvacadme@gmail.com>
2025-04-27 16:13:15 -0500

    Modifica connection string al nombre del servidor y su instancia. Agrega la primera migracion

---
+++ UniWay-WebAppMVC/Migrations/20250427210806_MigracionInicialUniway.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniWay_WebAppMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracionInicialUniway : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdBanner = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Contrasena = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    EsConductor = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehiculo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Marca = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Modelo = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Placa = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    ConductorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehiculo", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Vehiculo_Usuario_ConductorId",
+                        column: x => x.ConductorId,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Viaje",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Origen = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Destino = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    FechaHoraSalida = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Precio = table.Column<float>(type: "real", nullable: false),
+                    AsientosDisponibles = table.Column<int>(type: "int", nullable: false),
+                    ConductorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Viaje", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Viaje_Usuario_ConductorId",
+                        column: x => x.ConductorId,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reserva",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Estado = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    MetodoPago = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    ViajeId = table.Column<int>(type: "int", nullable: false),
+                    PasajeroId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reserva", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reserva_Usuario_PasajeroId",
+                        column: x => x.PasajeroId,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Reserva_Viaje_ViajeId",
+                        column: x => x.ViajeId,
+                        principalTable: "Viaje",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reserva_PasajeroId",
+                table: "Reserva",
+                column: "PasajeroId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reserva_ViajeId",
+                table: "Reserva",
+                column: "ViajeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vehiculo_ConductorId",
+                table: "Vehiculo",
+                column: "ConductorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Viaje_ConductorId",
+                table: "Viaje",
+                column: "ConductorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reserva");
+
+            migrationBuilder.DropTable(
+                name: "Vehiculo");
+
+            migrationBuilder.DropTable(
+                name: "Viaje");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
